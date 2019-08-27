6fba1be3f83ee1d73fd9dfaa260b36d61246f099
RossiniADS <rossini_rodrigues_alves@hotmail.com>
2019-08-26 21:04:55 -0300

    Atualizacao

--- OrientacaoObjeto/src/Lista02/Exercicio06.java
+++ OrientacaoObjeto/src/Lista02/Exercicio06.java
@@ -6,6 +6,37 @@ import entidadesLista02.Casa;
 public class Exercicio06 {
 
 	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		Casa casa = new Casa();
 
+		System.out.println("Porta 1: " + casa.isPorta1());
+		System.out.println("Porta 2: " + casa.isPorta2());
+		System.out.println("Porta 3: " + casa.isPorta3());
+		System.out.println("Cor....: " + casa.getCor());
+		
+		
+	/*	int opcao = 0;
+		do {
+			System.out.println("======================");
+			System.out.println("       M E N U");
+			System.out.println("======================");
+			System.out.println("1) Abrir porta");
+			System.out.println("2) Fechar porta");
+			System.out.println("3) Pintar a casa");
+			System.out.println("4) Pintar a casa (com menu de cores)");
+			System.out.println("5) Qtde portas abertas");
+			System.out.println("6) Sair");
+			System.out.println("---");
+			System.out.print(">>> Opcao: ");
+			opcao = entrada.nextInt();
+
+			switch (opcao) {
+			case 1:
+				System.out.println("\n\tAbrir porta\n");
+				System.out.print("Entre com o numero da porta: ");
+
+			}
+		} while (opcao != 6);*/
+		entrada.close();
 	}
 }
--- OrientacaoObjeto/src/entidadesLista02/Casa.java
+++ OrientacaoObjeto/src/entidadesLista02/Casa.java
@@ -1,5 +1,82 @@
 package entidadesLista02;
 
 public class Casa {
+	private boolean porta1, porta2, porta3;
+	private String cor;
 
+	public Casa() {
+		porta1 = porta2 = porta3 = false;
+		cor = "branca";
+	}
+
+	public boolean setPorta1(boolean porta1) {
+		// <classe>.<atributo> = <parametro>;
+
+		if (this.porta1 != porta1) {
+			this.porta1 = porta1;
+			return true;
+		}
+		return false;
+	}
+
+	public boolean setPorta2(boolean porta2) {
+		if (this.porta2 != porta2) {
+			this.porta2 = porta2;
+			return true;
+		}
+		return false;
+	}
+
+	public boolean setPorta3(boolean porta3) {
+		if (this.porta3 != porta3) {
+			this.porta3 = porta3;
+			return true;
+		}
+		return false;
+	}
+
+	public boolean setCor(String cor) {
+		// branca azul roxa;
+		if (!this.cor.equalsIgnoreCase(cor)) {
+			if (cor.equalsIgnoreCase("branca") || cor.equalsIgnoreCase("azul") || cor.equalsIgnoreCase("roxo")) {
+				this.cor = cor;
+			}
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isPorta1() {
+		return porta1;
+	}
+
+	public boolean isPorta2() {
+		return porta2;
+	}
+
+	public boolean isPorta3() {
+		return porta3;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	/*
+	 * public boolean abrirPorta() {
+	 * 
+	 * }
+	 * 
+	 * public boolean fecharPorta() {
+	 * 
+	 * }
+	 * 
+	 * public boolean pintar() {
+	 * 
+	 * }
+	 * 
+	 * public int quantasPortasEstaoAbertas() {
+	 * 
+	 * }
+	 */
 }
